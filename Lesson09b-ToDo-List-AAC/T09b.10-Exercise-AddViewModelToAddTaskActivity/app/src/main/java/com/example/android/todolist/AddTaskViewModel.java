54e55c3f062141ce6534b4bc736175639e122a4c
Ayush Maharjan <ius.maharjan@gmail.com>
2018-08-09 11:29:49 -0400

    Complete T09b.10

--- Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModel.java
+++ Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModel.java
@@ -1,12 +1,25 @@
 package com.example.android.todolist;
 
-// TODO (5) Make this class extend ViewModel
-public class AddTaskViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.util.Log;
 
-    // TODO (6) Add a task member variable for the TaskEntry object wrapped in a LiveData
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
 
-    // TODO (8) Create a constructor where you call loadTaskById of the taskDao to initialize the tasks variable
-    // Note: The constructor should receive the database and the taskId
+public class AddTaskViewModel extends ViewModel {
 
-    // TODO (7) Create a getter for the task variable
+    private LiveData<TaskEntry> task;
+    public static final String TAG = AddTaskViewModel.class.getSimpleName();
+
+    public AddTaskViewModel(AppDatabase database, int taskId) {
+        Log.d(TAG, "Actively retrieving a specific task from the DataBase");
+
+        task = database.taskDao().loadTaskById(taskId);
+    }
+
+    public LiveData<TaskEntry> getTask() {
+        return task;
+    }
+
 }
